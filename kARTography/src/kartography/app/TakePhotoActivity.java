cc8313c9444cf99943e4cb4c55878df7eff8ada0
Steven Dobek <sdobek47@gmail.com>
2014-02-15 14:44:18 -0800

    Can now take photo from the app.  Photo returns to the app when taken.

--- kARTography/src/kartography/app/TakePhotoActivity.java
+++ kARTography/src/kartography/app/TakePhotoActivity.java
@@ -1,16 +1,26 @@
 package kartography.app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import kartography.models.Location;
 import kartography.models.Poi;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class TakePhotoActivity extends Activity {
+	private static final int REQUEST_IMAGE_CAPTURE = 11;
 	Poi pntOfInterest;
 	Location loc;
 	ImageView photo;
@@ -19,6 +29,8 @@ public class TakePhotoActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_take_photo);
+		
+		photo = (ImageView) findViewById(R.id.iv_Photo);
 	}
 
 	@Override
@@ -32,6 +44,13 @@ public class TakePhotoActivity extends Activity {
 		//Intent to take photo using android camera
 		//Return data to store photo
 		Toast.makeText(getBaseContext(), "Loading Camera", Toast.LENGTH_SHORT).show();
+		
+		
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	    	takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri("photo.jpg"));
+	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	    }
 	}
 	
 	public void onSelectLocation(View v){
@@ -47,7 +66,36 @@ public class TakePhotoActivity extends Activity {
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		//Will return intent data for taking photo/poi and setting location
+	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	        Uri takenPhotoUri = getPhotoFileUri("photo.jpg");
+	        Bitmap imageBitmap;
+			try {
+				imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), takenPhotoUri);
+				photo.setImageBitmap(imageBitmap);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	    }
 	}
+	
+	public Uri getPhotoFileUri(String fileName) {
+		  File mediaStorageDir = new File(
+		        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+		        "MyCameraApp");
+
+		    // Create the storage directory if it does not exist
+		    if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+		        Log.d("MyCameraApp", "failed to create directory");
+		    }
+		    // Specify the file target for the photo
+		    Uri fileUri = Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator +
+		                fileName));
+		    return fileUri;
+		}
 
 }
