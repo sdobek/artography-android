6b925dd942d7318434bfe4b210a19b19b319c183
Steven Dobek <sdobek47@gmail.com>
2014-02-16 22:40:19 -0800

    Facebook login code.  Requires FacebookSDK in properties to work.

---
+++ kARTography/src/kartography/app/LoginActivity.java
@@ -0,0 +1,82 @@
+package kartography.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.ParseException;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+public class LoginActivity extends Activity {
+
+	private Button loginButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		Parse.initialize(this, "wN6gpXkwVEF0d9eTw1YzE0ISX2WM8ACdXM0ueuiu",
+				"dGycMyN2IxdihwSV6kzDiCufYAL9UBBQEpOiRmMn");
+		ParseFacebookUtils.initialize(getString(R.string.app_id));
+
+		loginButton = (Button) findViewById(R.id.btn_login);
+		// Check if there is a currently logged in user
+		// and they are linked to a Facebook account.
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if ((currentUser != null) && ParseFacebookUtils.isLinked(currentUser)) {
+			// Go to the user info activity
+			showMainActivity();
+		}
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+	}
+
+	private void showMainActivity() {
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	public void onLogIn(View v) {
+		List<String> permissions = Arrays.asList("basic_info", "user_about_me");
+		ParseFacebookUtils.logIn(permissions, this, new LogInCallback() {
+			@Override
+			public void done(ParseUser user, com.parse.ParseException e) {
+				if (user == null) {
+					Log.d("FB_LOGIN","Uh oh. The user cancelled the Facebook login.");
+				} else if (user.isNew()) {
+					Log.d("FB_LOGIN","User signed up and logged in through Facebook!");
+					showMainActivity();
+				} else {
+					Log.d("FB_LOGIN","User logged in through Facebook!");
+					showMainActivity();
+				}
+
+			}
+
+		});
+	}
+
+}
--- kARTography/src/kartography/app/MainActivity.java
+++ kARTography/src/kartography/app/MainActivity.java
@@ -1,11 +1,14 @@
 package kartography.app;
 
-
 import java.util.Date;
 
 import kartography.models.Poi;
 import kartography.models.PoiLocation;
 import kartography.models.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
@@ -16,37 +19,100 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.facebook.FacebookRequestError;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.model.GraphUser;
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
 public class MainActivity extends FragmentActivity implements TabListener {
 
 	Poi art;
 	User user;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		ParseObject.registerSubclass(Poi.class);
-		Parse.initialize(this, "wN6gpXkwVEF0d9eTw1YzE0ISX2WM8ACdXM0ueuiu", "dGycMyN2IxdihwSV6kzDiCufYAL9UBBQEpOiRmMn");
+//		Parse.initialize(this, "wN6gpXkwVEF0d9eTw1YzE0ISX2WM8ACdXM0ueuiu",
+//				"dGycMyN2IxdihwSV6kzDiCufYAL9UBBQEpOiRmMn");
 		setContentView(R.layout.activity_main);
-		   
-		makeTestPoiObjectandUser();
-		ParseObject testObject = new ParseObject("TestObject");
-		
-		testObject.put("foo", "bar");
-		//right now these crash the app :( will 
-		//try to understand parse more. 
-//		testObject.put("artObjectFoo", art);
-//		testObject.add("user", user);
-		testObject.saveInBackground();
-		
+
+		// Fetch Facebook user info if the session is active
+		Session session = ParseFacebookUtils.getSession();
+		if (session != null && session.isOpened()) {
+			getFacebookInfo();
+		}
+
 		setupNavigationTabs();
+
+	}
+
+	private void getFacebookInfo() {
+		Request request = Request.newMeRequest(ParseFacebookUtils.getSession(),
+				new Request.GraphUserCallback() {
+					@Override
+					public void onCompleted(GraphUser user, Response response) {
+						if (user != null) {
+							// Create a JSON object to hold the profile info
+							JSONObject userProfile = new JSONObject();
+							try {
+								// Populate the JSON object
+								userProfile.put("facebookId", user.getId());
+								userProfile.put("name", user.getName());
+								updateViewsWithProfileInfo();
+							} catch (JSONException e) {
+								Log.d("FB_LOGIN","Error parsing returned user data.");
+							}
+
+						} else if (response.getError() != null) {
+							if ((response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_RETRY)
+									|| (response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_REOPEN_SESSION)) {
+								Log.d("FB_LOGIN","The facebook session was invalidated.");
+								onLogoutButtonClicked();
+							} else {
+								Log.d("FB_LOGIN","Some other error: "
+												+ response.getError()
+														.getErrorMessage());
+							}
+						}
+					}
+				});
+		request.executeAsync();
+
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+			// Check if the user is currently logged
+			// and show any cached content
+			updateViewsWithProfileInfo();
+		} else {
+			// If the user is not logged in, go to the
+			// activity showing the login view.
+			startLoginActivity();
+		}
+	}
+	
+	/*
+	 * If we need to display in main activity
+	 */
+	private void updateViewsWithProfileInfo() {
+		// TODO Auto-generated method stub
 		
 	}
 
@@ -55,32 +121,34 @@ public class MainActivity extends FragmentActivity implements TabListener {
 		String profileURL = "https://pbs.twimg.com/profile_images/378800000504479551/6e237aa9c711a6d3b23ff2ed07e09648.png";
 		String graffitiURL = "http://www.thisiscolossal.com/wp-content/uploads/2012/04/tfs-1.jpg";
 		user = new User("thatdood", "johnny", "comelately", date, profileURL);
-		Long longitude =  Long.getLong("37.792962");
+		Long longitude = Long.getLong("37.792962");
 		Long latidude = Long.getLong("-122.483236");
 		PoiLocation location = new PoiLocation(longitude, latidude);
-		art = new Poi("someart", "someartist", date, "test", graffitiURL, user, location);
-		
+		art = new Poi("someart", "someartist", date, "test", graffitiURL, user,
+				location);
+
 	}
 
 	private void setupNavigationTabs() {
 
 		ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
-		actionBar.setDisplayShowTitleEnabled(true);
-		
-		Tab tabHome = actionBar.newTab().setText("List").setTag("PoiListFragment")
-				.setIcon(R.drawable.ic_launcher).setTabListener(this);
 
-		Tab tabMentions = actionBar.newTab().setText("Map").setTag("PoiMapFragment")
-				.setIcon(R.drawable.ic_launcher).setTabListener(this);
-		
+		actionBar.setDisplayShowTitleEnabled(true);
+
+		Tab tabHome = actionBar.newTab().setText("List")
+				.setTag("PoiListFragment").setIcon(R.drawable.ic_launcher)
+				.setTabListener(this);
+
+		Tab tabMentions = actionBar.newTab().setText("Map")
+				.setTag("PoiMapFragment").setIcon(R.drawable.ic_launcher)
+				.setTabListener(this);
+
 		actionBar.addTab(tabHome);
 		actionBar.addTab(tabMentions);
-				
+
 		actionBar.selectTab(tabHome);
-		
-		
+
 	}
 
 	@Override
@@ -89,7 +157,7 @@ public class MainActivity extends FragmentActivity implements TabListener {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	public void addNewArt(MenuItem m) {
 		Intent i = new Intent(this, TakePhotoActivity.class);
 		startActivity(i);
@@ -99,7 +167,7 @@ public class MainActivity extends FragmentActivity implements TabListener {
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -108,30 +176,53 @@ public class MainActivity extends FragmentActivity implements TabListener {
 		android.support.v4.app.FragmentTransaction fts = manager
 				.beginTransaction();
 
-		//lazy instantiation for the win
+		// lazy instantiation for the win
 		if (tab.getTag() == "PoiListFragment") {
 			fts.replace(R.id.frameContainer, new PoiListFragment());
-//			if (htlFrag == null) {
-//				htlFrag = new HomeTimelineFragment();
-//			}
-			
-//			fts.replace(R.id.frameContainer, htlFrag, "HTL");
+			// if (htlFrag == null) {
+			// htlFrag = new HomeTimelineFragment();
+			// }
+
+			// fts.replace(R.id.frameContainer, htlFrag, "HTL");
 		} else {
 			fts.replace(R.id.frameContainer, new PoiMapFragment());
-//			if (mFrag == null) {
-//				mFrag = new MentionsFragment();
-//			}
-//			fts.replace(R.id.frameContainer, mFrag, "MF");
+			// if (mFrag == null) {
+			// mFrag = new MentionsFragment();
+			// }
+			// fts.replace(R.id.frameContainer, mFrag, "MF");
 		}
 		fts.commit();
 	}
-		
-	
 
 	@Override
 	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode,
+				resultCode, data);
+	}
+
+	/*
+	 * If we want a logout button
+	 */
+	private void onLogoutButtonClicked() {
+		// Log the user out
+		ParseUser.logOut();
+
+		// Go to the login view
+		startLoginActivity();
+	}
+
+	private void startLoginActivity() {
+		Intent intent = new Intent(this, LoginActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
 	}
 
 }
